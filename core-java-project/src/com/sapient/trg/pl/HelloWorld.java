5bb78906a0c5db24ada4e98fc7bb53f9587698df
kunalchaurasiya07 <kunalchaurasiya75@gmail.com>
2021-04-19 16:52:10 +0530

    first commit in ps

---
+++ core-java-project/src/com/sapient/trg/pl/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.sapient.trg.pl;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * This is java doc comment
+ * @author kunchaur
+ * demo class
+ *
+ */
+
+public class HelloWorld {
+	/**
+	 * 
+	 * @param args
+	 * This is main() method
+	 */
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// This is signal class comment
+		/*
+		 * This 
+		 * is 
+		 * multiline 
+		 * comment
+		 */
+       System.out.println("Hello World!");
+       System.out.println("Today's date: "+ new Date());
+       System.out.println("Today: "+ Calendar.getInstance());
+	}
+}
